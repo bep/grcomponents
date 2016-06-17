ab2e94b4a02b54822d7575c6ae081c4bbca8b951
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2016-06-17 10:08:48 +0200

    collapsible: Add options

--- collapsible/collapsible.go
+++ collapsible/collapsible.go
@@ -11,12 +11,31 @@ const toggleKey = "toggleState"
 
 type toggle struct {
 	*gr.This
+
+	o Options
 }
 
-// New creates a new toggle wrapper.
-// TODO(bep) options: text, style, wrapper element.
+// Options provides optional options. All of these have sane defaults.
+type Options struct {
+	OnClass           string
+	OffClass          string
+	OnText            string
+	OffText           string
+	OffPanelClass     string
+	OnPanelClass      string
+	OffPanelBodyClass string
+	OnPanelBodyClass  string
+}
+
+// New creates a new toggle wrapper with sane defaults.
 func New() *gr.ReactComponent {
 	return gr.New(&toggle{})
+}
+
+// NewWith cretes a new toggle wrapper with the given options.
+// TODO(bep) options: wrapper element.
+func NewWith(options Options) *gr.ReactComponent {
+	return gr.New(&toggle{o: options})
 }
 
 func (c *toggle) Render() gr.Component {
@@ -24,20 +43,45 @@ func (c *toggle) Render() gr.Component {
 	var (
 		toggleStyle gr.Modifier
 		buttonText  string
+
+		panelClass     gr.Modifier
+		panelBodyClass gr.Modifier = gr.Discard
 	)
 
 	enabled := c.State().Bool(toggleKey)
 
 	if enabled {
-		toggleStyle = gr.Style("display", "block")
-		buttonText = "Hide"
+		if c.o.OnClass != "" {
+			toggleStyle = gr.CSS(c.o.OnClass)
+		} else {
+			toggleStyle = gr.Style("display", "block")
+		}
+
+		buttonText = def(c.o.OnText, "Hide")
+
+		panelClass = gr.CSS(def(c.o.OnPanelClass, "panel-default"))
+
+		if c.o.OnPanelBodyClass != "" {
+			panelBodyClass = gr.CSS(c.o.OnPanelBodyClass)
+		}
+
 	} else {
-		toggleStyle = gr.Style("display", "none")
-		buttonText = "Show"
+		if c.o.OffClass != "" {
+			toggleStyle = gr.CSS(c.o.OffClass)
+		} else {
+			toggleStyle = gr.Style("display", "none")
+		}
+		buttonText = def(c.o.OffText, "Show")
+
+		if c.o.OffPanelBodyClass != "" {
+			panelBodyClass = gr.CSS(c.o.OffPanelBodyClass)
+		}
+
+		panelClass = gr.CSS(def(c.o.OffPanelClass, "panel-default"))
 	}
 
 	return el.Div(
-		gr.CSS("panel panel-default"),
+		gr.CSS("panel"), panelClass,
 		el.Div(
 			gr.CSS("panel-heading"),
 			el.Header4(
@@ -52,6 +96,7 @@ func (c *toggle) Render() gr.Component {
 			toggleStyle,
 			el.Div(
 				gr.CSS("panel-body"),
+				panelBodyClass,
 				c.Children().Element(),
 			),
 		),
@@ -65,4 +110,11 @@ func (c *toggle) GetInitialState() gr.State {
 
 func (c *toggle) onClick(event *gr.Event) {
 	c.SetState(gr.State{toggleKey: !c.State().Bool(toggleKey)})
+}
+
+func def(v, d string) string {
+	if v != "" {
+		return v
+	}
+	return d
 }
